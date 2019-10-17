0408db09a10ff3cacc1478bf057db1e5e474aa09
nnta27812 <55732504+nnta27812@users.noreply.github.com>
2019-10-17 12:28:05 +0700

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Problem1
+{
+    public class Account
+    {
+        int money = 200000;
+
+        public Account(int amount)
+        {
+            money = amount;
+        }
+        public void Deposit(int amount)
+        {
+            money += amount;
+            Console.WriteLine("The amount you Deposit is : " + money + "\nThanks you for use our service . Have a nice day!");
+            
+        }
+        public void Withdraw(int amount)
+        {
+            money -= amount;
+            Console.WriteLine("The amount you Withdraw is : " + money + "\nThanks you for use our service . Have a nice day!");
+         
+        }
+        public int Balance()
+        {
+            Console.WriteLine("The amount of your is : " + money + "\nThanks you for use our service . Have a nice day!");
+            return money;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there , We have three service for you : ");
+
+            Console.WriteLine(" 1 for Deposit");
+            Console.WriteLine(" 2 for Withdraw");
+            Console.WriteLine(" 3 for check your Balance");
+
+            Console.WriteLine("What service do you want ?");
+            
+            int result;
+
+            if (int.TryParse(Console.ReadLine(), out result))
+            {
+                if(result == 1)
+                {
+                    Account acc = new Account(200000);
+                    Console.WriteLine("Please enter your money you want to deposit : ");
+                    string str = Console.ReadLine();
+                    int a = Convert.ToInt32(str);
+                    acc.Deposit(a);
+                }
+                else if (result == 2)
+                {
+                    Account acc = new Account(200000);
+                    Console.WriteLine("Please enter your money you want to withdraw : ");
+                    string str1 = Console.ReadLine();
+                    int a = Convert.ToInt32(str1);
+                    acc.Withdraw(a);
+                }
+                else if ( result == 3)
+                {
+                    Account acc = new Account(200000);
+                    acc.Balance();
+                }
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
